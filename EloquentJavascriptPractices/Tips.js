53637105bbaa447f70cef8d1453ef0262c81a993
KhizerRehan <KhizerRehan92@gmail.com>
2017-01-19 01:08:58 +0500

    Chp 1-2 completed

---
+++ EloquentJavascriptPractices/Tips.js
@@ -0,0 +1,13 @@
+// =================================
+// =================================
+// Capitalization
+// Variable names may not contain spaces, yet it is often helpful to use
+// multiple words to clearly describe what the variable represents. These
+// are pretty much your choices for writing a variable name with several
+// words in it:
+fuzzylittleturtle
+fuzzy_little_turtle
+FuzzyLittleTurtle
+fuzzyLittleTurtle
+// =================================
+// =================================
